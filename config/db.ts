5f46436e14540093948bbab4c7197b9fdb27ffa2
Rudrava Mukherjee <rudravamukherjee.2001@gmail.com>
2024-01-28 22:04:45 +0530

    feat: add typeorm :fire:

--- config/db.ts
+++ config/db.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export const dbConfig = registerAs('database', () => ({
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT || 5432,
+  port: process.env.DATABASE_PORT || 7482,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   name: process.env.DB_NAME,
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { baseConfig, dbConfig } from 'config';
 import { validationSchema } from 'config/validator';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './db.module';
 
 @Module({
   imports: [
@@ -13,6 +16,8 @@ import { validationSchema } from 'config/validator';
         abortEarly: true,
       },
     }),
+    DatabaseModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
